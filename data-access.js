86f42459b9fcc88123e89d4dabf0b38062bde4f2
nalini-purushothaman <pnalini90sri@gmail.com>
2024-04-24 14:25:00 -0700

    completed stage 02 add getCustomer

---
+++ data-access.js
@@ -0,0 +1,35 @@
+// mongodb client driver
+const { MongoClient } = require("mongodb");
+
+// DB Connection URL
+var url = "mongodb://127.0.0.1:27017";
+
+// Database and collection variables
+const dbName = "custdb";
+const collectionName = "customers";
+const connectString = url + "/" + dbName;
+let collection;
+
+async function dbStartup() {
+  // Create client
+  const client = new MongoClient(connectString);
+  await client.connect();
+  // set the database to use
+  const db = client.db(dbName);
+  // set the collection to use
+  collection = db.collection(collectionName);
+}
+
+//get all customers
+async function getCustomers() {
+  try {
+    const customers = await collection.find({}).toArray();
+    return [customers,null];
+  } catch (err) {
+    console.log(err.message);
+    return [null,err.message];
+  }
+}
+
+dbStartup();
+module.exports = { getCustomers };
