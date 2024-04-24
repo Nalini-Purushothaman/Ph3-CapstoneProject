06fbff676ad7ae080c31063eed24e3021819474b
nalini-purushothaman <pnalini90sri@gmail.com>
2024-04-24 10:44:32 -0700

    completed stage01 basic file server

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
-import express from "express";
-//const path=require('path');
-import path from "path";
+const express = require("express");
+const path = require("path");
 
 const app = express();
 const PORT = 4000;
