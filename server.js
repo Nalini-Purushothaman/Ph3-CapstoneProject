44c2a598caf7a36d22322c8c2d813dab9e60854b
nalini-purushothaman <pnalini90sri@gmail.com>
2024-04-24 14:50:18 -0700

    completed stage03 add error handling

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 const express = require("express");
 const path = require("path");
+const da = require("./data-access");
 
 const app = express();
 const PORT = 4000;
 app.use(express.static(path.join(__dirname, "public")));
 app.listen(PORT, () => console.log(`Your server is running on port ${PORT}`));
+
+app.get("/customers", async (req, res) => {
+  const [cust, err] = await da.getCustomers();
+  if (cust != null) {
+    res.send(cust);
+  } else {
+    res.status(500).send(err);
+  }
+});
